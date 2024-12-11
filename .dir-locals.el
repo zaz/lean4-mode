46229165517894337ec014ca24b3f017cef8ef30
Mekeor Melire <mekeor@posteo.de>
2024-12-11 21:08:49 +0100

    Do not use tabs in Elisp code
    Replace tabs with spaces.  Reindent changed code.  Directory-locally
    set indent-tabs-mode to nil for emacs-lisp-mode.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
-;; -*- no-byte-compile: t; -*-
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
 
-((nil
-  (bug-reference-url-format
-   . "https://github.com/leanprover-community/lean4-mode/issues/%s")))
+((nil . ((bug-reference-url-format . "https://github.com/leanprover-community/lean4-mode/issues/%s")))
+ (emacs-lisp-mode . ((indent-tabs-mode . nil))))
